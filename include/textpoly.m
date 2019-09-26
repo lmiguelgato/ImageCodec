2f6f65169971959804a8393d6fbed32d612dd384
Luis M. Gato <lmiguelgato@gmail.com>
2019-09-26 15:06:37 -0500

    Convert binary vector of polynomial coefficients to polynomial text

---
+++ include/textpoly.m
@@ -0,0 +1,26 @@
+function T = textpoly( P )
+% Represent coding polynomial as text.
+%
+% @input:   P, a binary vector of polynomial coefficients.
+%
+% @output:  T, the polynomial represented as text.
+
+m = length(P) - 1;
+
+T = '';
+for i = m+1:-1:3
+    if P(m-i+2) == 1
+       T =  strcat(T, ['D^' num2str(i-1) '+']);
+    end
+end
+if P(m) == 1
+    T =  strcat(T, 'D+');
+end
+if P(m+1) == 1
+    T =  strcat(T, '1');
+else
+    T = T(1:end-1);
+end
+
+end
+
