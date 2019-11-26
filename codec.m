e0df7d14728b2e05c99df7ca684e4010614561c7
Luis M. Gato <lmiguelgato@gmail.com>
2019-11-25 22:45:42 -0600

    Update codec

--- codec.m
+++ codec.m
@@ -40,18 +40,19 @@ image_dim = M*N*L;
 %% convert image to a matrix of raw bits:
 disp('Converting image to raw bits ...')
 tic;
-[B, P] = image2bits(I.data);
+[B, ~, P] = image2bits(I.data);
 dt = toc;
 disp([num2str(dt) ' s'])
-message_len = size(B,1);    % M*N*P
+message_len = size(B,1);    % M*N*P x 1
 
 %% channel coding:
 switch codingScheme
     case 1
         % Hamming encoder
-        m = 3;                      % the smaller the better error correction, but
-                                    % the lower code efficiency
-        disp(['Hamming encoder using polynomial ' textpoly(gfprimdf(m)) ' ...'])
+        m = 3;              % the smaller the better error correction, but
+                            % the lower code efficiency
+        disp(['Hamming encoder using polynomial ' ...
+            textpoly(gfprimdf(m)) ' ...'])
         tic;
         [B, C, k, n, h, num_of_blocks] = hamming_encoder(m, B);
         disp(['Coder efficiency:  ' num2str(k/n)])
@@ -59,11 +60,12 @@ switch codingScheme
         disp([num2str(dt) ' s'])
     case 2
         % BCH encoder
-        m = 5;
-        k = 16;       % Message length
+        m = 6;
+        k = 10;       % Message length
         n = 2^m-1;   % Codeword length
         t = bchnumerr(n,k);
-        disp(['BCH encoder using polynomial ' textpoly(bchgenpoly(n,k)) ' ...'])
+        disp(['BCH encoder using polynomial ' ...
+            textpoly(bchgenpoly(n,k)) ' ...'])
         disp(['Error-correction capability:  ' num2str(t)])
         disp(['Coder efficiency:  ' num2str(k/n)])
         tic;
@@ -76,9 +78,9 @@ end
 %% channel model:
 disp('Modeling channel ...')
 tic;
-ber = 5e-3;                             % bit error rate
+ber = 1e-1;                             % bit error rate
 C_r = bsc(C, ber);                      % binary symmetric channel
-[numerrs, pcterrs] = biterr(C, C_r);    % number of errors and actual ber
+[numerrs, pcterrs] = count_errors(C, C_r);    % number of errors and actual ber
 dt = toc;
 disp([num2str(dt) ' s'])
 
@@ -143,8 +145,15 @@ title(['Without channel coding: BER = ' num2str(pcterrs)])
 
 subplot 122
 imshow(I_d)
-title(['With Hamming(' num2str(n) ',' ...
-    num2str(k) ') channel coding: BER = ' num2str(ber_d)])
+
+switch codingScheme
+    case 1        
+        title(['With Hamming(' num2str(n) ',' ...
+        num2str(k) ') channel coding: BER = ' num2str(ber_d)])
+    case 2
+        title(['With BCH(' num2str(n) ',' ...
+        num2str(k) ') channel coding: BER = ' num2str(ber_d)])
+end
 
 figure
 imshow(I.data)
